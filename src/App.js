88171737b0a5c9931f25cdd1f6ef713d29c19efb
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2022-07-23 17:26:51 +0530

    weather aap ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <div className="App">
-    <h1>starting the build Weather app</h1>
+     <Weather/>
     </div>
   );
 }
